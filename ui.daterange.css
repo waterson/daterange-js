19d7eb5222f36f414fa18897a2b5445d2bbc6463
Chris Waterson <waterson@maubi.net>
2012-06-12 16:41:11 -0700

    first checked in.

---
+++ ui.daterange.css
@@ -0,0 +1,20 @@
+/* Note use of IE "star hack" in a couple of these rules. */
+.dr-dropdown { display: inline-block; *display: inline; *position:absolute; background: url(calendar-icon.gif) 50% 50% no-repeat; width: 20px; height: 20px; vertical-align: bottom; cursor: pointer; }
+.dr-dropdown.dr-open { background-image: url(calendar-icon.gif); }
+.dr-panel { position: absolute; padding: 10px; border: 1px solid #888; display: none; background: #fff; z-index: 1000; font-size: 12px; line-height: normal; width: 470px; *width: 500px; }
+.dr-scroll { float: left; width: 20px; height: 20px; cursor: pointer; }
+.dr-left { background: url(arrow-black-left.png) 50% 50% no-repeat; }
+.dr-right { background: url(arrow-black-right.png) 50% 50% no-repeat; }
+.dr-calendar-container { float: left; position: relative; width: 430px; height: 150px; overflow: hidden; }
+.dr-calendar { position: absolute; width: 10000px; }
+.dr-month { float: left; margin-right: 4px; background: #eee; border-bottom: 1px solid #eee !important;}
+.dr-month-name { text-align: center; font-weight: bold; padding: 0.25em 0; }
+.dr-week { clear: both; }
+.dr-day, .dr-date { width: 1em; *width: 1.5em; float: left; padding: 2px 0.25em 0 0.25em; margin: 1px 1px 0 1px; background: #fff; text-align: center; }
+.dr-date { cursor: pointer; }
+.dr-date.dr-overflow { color: #888; }
+.dr-date.dr-future, .dr-date.dr-overflow.dr-future { color: #ccc; }
+.dr-date.dr-today { font-weight: bold; }
+.dr-date.dr-inrange { color: #fff; background-color: #48f; }
+.dr-date.dr-overflow.dr-inrange { background-color: #acf; }
+.dr-close { display: block; clear: both; }
